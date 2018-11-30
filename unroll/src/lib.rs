08be30b4dcca76d1abf900ea4ca055d924f9fb97
AuroransSolis <primalucegd@gmail.com>
2018-11-30 09:56:41 -0700

    Imported necessary crates.

--- unroll/src/lib.rs
+++ unroll/src/lib.rs
@@ -1,7 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate proc_macro;
+#[macro_use] extern crate syn;
+#[macro_use] extern crate quote;
+#[macro use] extern crate sll;
+
+use sll::SLL;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::parse::{Parse, ParseStream, Result};
+use syn::{parse_macro_input, ExprRange, ExprForLoop, Expr, Pat, Block, ExprWhile};
+
+#[proc_macro]
+pub fn unroll_for(input: TokenStream) -> TokenStream {
+
+}
+
+#[proc_macro]
+pub fn unroll_while(input: TokenStream) -> TokenStream {
+
 }
